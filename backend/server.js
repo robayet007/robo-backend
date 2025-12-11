963b4f26db3ad3257dd971e7816bc1f11f33de21
robayet007 <mdrobayet007@gmail.com>
2025-12-12 03:14:23 +0600

    done backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'https://robotopup.vercel.app/' , 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
